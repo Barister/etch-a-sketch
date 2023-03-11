b3463548660334a32df3fac153b40d4c471c85d6
Barister <59793749+Barister@users.noreply.github.com>
2023-03-11 18:31:26 +0300

    JS made display grid changing sizes with using slider

--- js/script.js
+++ js/script.js
@@ -1,32 +1,95 @@
-// to determine some constant variables
+// to determine some variables
 
 const display = document.querySelector('.middle__display');
 
-console.log(display);
+const displayResolutionWidth = display.offsetWidth;
+const displayResolutionHeight = display.offsetHeight;
 
 
 
+console.log('display.children:', display.children);
 
-for (let i = 0; i < 9; i++) {
-    const displayRow = document.createElement('div')
-    displayRow.setAttribute('class', 'display__row')
-    displayRow.style.display = 'flex';
-    displayRow.style.justifyContent = 'space-between';
-    displayRow.style.height = 405/9+'px';
-    display.appendChild(displayRow);
-    console.log('i:', i);
-    for (let j = 0; j < 16; j++) {
-        const displayElement = document.createElement('div')
-        displayElement.setAttribute('class', 'display__element');
-        displayElement.style.width = 720 / 16+'px';
-        displayElement.style.height = 405/9+'px';
-        displayElement.style.border = '1px solid black';
-        displayRow.appendChild(displayElement);
-        console.log('j:', j);
+// to clear displayGrid
+
+function clearGrid() {
+    document.querySelectorAll('.display__row')
+     
+   .forEach(element => element.remove());
+ 
+}
+
+// to make a grid for display
+function makeGrid() {
+    let displayWidth = document.querySelector('.resolution__width').innerHTML / 1;
+    let displayHeight = document.querySelector('.resolution__height').innerHTML / 1;
+
+    for (let i = 0; i < displayHeight; i++) {
+        const displayRow = document.createElement('div');
+        displayRow.setAttribute('class', 'display__row');
+        displayRow.style.display = 'flex';
+        displayRow.style.justifyContent = 'space-between';
+        displayRow.style.height = displayResolutionHeight / displayHeight + 'px';
+        display.appendChild(displayRow);
+        
+        for (let j = 0; j < displayWidth; j++) {
+            const displayElement = document.createElement('div')
+            displayElement.setAttribute('class', 'display__element');
+            displayElement.style.width = displayResolutionWidth / displayWidth + 'px';
+            // displayElement.style.border = '1px solid black';
+            displayRow.appendChild(displayElement);
+            
+        }
+    }
+}
+
+makeGrid();
+makeHover()
+
+// to make slider change of grid
+
+
+function sizeChange() {
+    let sizeSlider = document.querySelector('.vertical__slider');
+    let eraseSlider = document.querySelector('.clear__slider');
+
+    let spanWidth = document.querySelector('.resolution__width');
+    let spanHeight = document.querySelector('.resolution__height'); 
+
+    spanWidth.innerHTML = sizeSlider.value;
+    spanHeight.innerHTML = sizeSlider.value / 16 * 9;
+    
+    eraseSlider.setAttribute('max', sizeSlider.value);
+
+    clearGrid();
+    makeGrid();
+    makeHover()
+}
+
+
+
+// to make hover effect
+function makeHover() {
+    let displayElement = document.querySelectorAll('.display__element');
+
+    displayElement.forEach(element => element.addEventListener('mousemove', () => {
+        element.style.backgroundColor = '#000';
+    }));
+}
+
+
+// to erase display
+
+function eraseDisplay() {
+    let eraseSlider = document.querySelector('.clear__slider');
+
+    let displayElement = document.querySelectorAll('.display__element');
+
+    console.log('eraseSliderValue', eraseSlider.value);
+    
+    if (eraseSlider.value / 1 == eraseSlider.getAttribute('max')) {
+        displayElement.forEach(element => element.style.backgroundColor = '');
+
     }
 
     
-    
 }
-
-console.log(display);
